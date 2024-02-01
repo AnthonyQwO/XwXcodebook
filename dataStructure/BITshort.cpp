3df6619be32ed79f18dfd814ed0ad2cdb2200baf
AnthonyQwO <anthonyaa0423@gmail.com>
2024-02-01 21:15:46 +0800

    Update a short BIT.

---
+++ dataStructure/BITshort.cpp
@@ -0,0 +1,20 @@
+#define lowbit(x) (x&-x)
+struct BIT {
+    int n;
+    vector<int> bit;
+    BIT(int _n):n(_n), bit(n+1) {}
+    void update( int x, int val ) {
+        for(;x <= n; x += lowbit(x)) bit[x] += val;
+    }
+    void range_update( int L, int R, int val ) {
+        update(L,val), update(R+1,-val);        
+    }
+    int query( int x ) {
+        int res = 0;
+        for(;x; x -= lowbit(x)) res += bit[x];
+        return res;
+    }
+    int range_query( int L, int R ) {
+        return query(R)-query(L-1);
+    }
+};
