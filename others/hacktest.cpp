4e1af28ba3e09a718b77edae9cda848a4274337a
AnthonyQwO <anthonyaa0423@gmail.com>
2024-02-22 19:26:57 +0800

    Upload a template.

---
+++ others/hacktest.cpp
@@ -0,0 +1,12 @@
+/*
+Correct: 裁判接受提交的程式碼。
+Compiler-Error: 程式碼無法成功編譯。
+TimeLimit: 提交的程式太過耗時。
+Run-Error: 提交的程式結束時的 return code 不為零，通常是因為程式因權限錯誤遭作業系統終結。
+Wrong-Answer: 裁判拒絕接受提交的程式碼，因輸出不正確。
+No-Output: 提交的程式沒有輸出。
+*/
+if(/*TLE*/) for(;;);
+else if(/*WA*/) cout << "1"
+else if(/*RE*/) assert(0);
+else if(/*NoOutput*/) return 0;
