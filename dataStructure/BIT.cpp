9504791979034be1330058ffb42a3949380a1882
AnthonyQwO <anthonyaa0423@gmail.com>
2024-05-08 22:05:10 +0800

    Refactor template.

--- dataStructure/BIT.cpp
+++ dataStructure/BIT.cpp
@@ -2,19 +2,19 @@
 struct BIT {
     int n;
     vector<int> bit;
-    BIT(int _n):n(_n), bit(n+1) {}
-    void update( int x, int val ) {
-        for(;x <= n; x += lowbit(x)) bit[x] += val;
+    BIT(int _n):n(_n), bit(n + 1) {}
+    void update(int x, int val) {
+        for(; x <= n; x += lowbit(x)) bit[x] += val;
     }
-    void range_update( int L, int R, int val ) {
-        update(L,val), update(R+1,-val);        
+    void update(int L, int R, int val) {
+        update(L, val), update(R + 1, -val);        
     }
-    int query( int x ) {
+    int query(int x) {
         int res = 0;
-        for(;x; x -= lowbit(x)) res += bit[x];
+        for(; x; x -= lowbit(x)) res += bit[x];
         return res;
     }
-    int range_query( int L, int R ) {
-        return query(R)-query(L-1);
+    int query(int L, int R) {
+        return query(R) - query(L - 1);
     }
 };
--- dataStructure/seg_treewithtestcase.cpp
+++ dataStructure/seg_treewithtestcase.cpp
@@ -7,76 +7,67 @@ struct segmentTree {
     int n;
     vector<int> seg, tag, cov;
     segmentTree(int _n): n(_n) {
-        seg=tag=cov=vector<int>(n*4,0);
+        seg = tag = cov = vector<int>(n * 4, 0);
     }
     void push(int i, int L, int R) {
         if(cov[i]) {
-            seg[i]=cov[i]*(R-L+1);
+            seg[i] = cov[i] * (R - L + 1);
             if(L < R) {
-                cov[cl(i)]=cov[cr(i)]=cov[i];
-                tag[cl(i)]=tag[cr(i)]=0;
+                cov[cl(i)] = cov[cr(i)] = cov[i];
+                tag[cl(i)] = tag[cr(i)] = 0;
             }
-            cov[i]=0;
+            cov[i] = 0;
         }
         if(tag[i]) {
-            seg[i]+=tag[i]*(R-L+1);
+            seg[i] += tag[i] * (R - L + 1);
             if(L < R) {
-                tag[cl(i)]+=tag[i];
-                tag[cr(i)]+=tag[i];
+                tag[cl(i)] += tag[i];
+                tag[cr(i)] += tag[i];
             }
-            tag[i]=0;
+            tag[i] = 0;
         }
     }
     void pull(int i, int L, int R) {
         if(L >= R) return;
-        int mid=(L+R)>>1;
-        push(cl(i),L,mid);
-        push(cr(i),mid+1,R);
-        seg[i]=seg[cl(i)]+seg[cr(i)];
+        int mid = L + R >> 1;
+        push(cl(i), L, mid);
+        push(cr(i), mid + 1, R);
+        seg[i] = seg[cl(i)] + seg[cr(i)];
     }
-    void build(vector<int>& arr, int i=1, int L=1, int R=-1) {
-        if(R == -1) R=n;
-        if(L == R) {
-            seg[i]=arr[L];
-            return;
-        }
-        int mid=(L+R)>>1;
-        build(arr,cl(i),L,mid);
-        build(arr,cr(i),mid+1,R);
-        pull(i,L,R);
+    void build(vector<int>& arr, int i = 1, int L = 1, int R = -1) {
+        if(R == -1) R = n;
+        if(L == R) return void(seg[i] = arr[L]);
+        int mid = L + R >> 1;
+        build(arr, cl(i), L, mid);
+        build(arr, cr(i), mid + 1, R);
+        pull(i, L, R);
     }
-    int query(int rL, int rR, int i=1, int L=1, int R=-1) {
-        if(R == -1) R=n;
-        push(i,L,R);
+    int query(int rL, int rR, int i = 1, int L = 1, int R = -1) {
+        if(R == -1) R = n;
+        push(i, L, R);
         if(rL <= L && R <= rR) return seg[i];
-        int mid=(L+R)>>1, ret=0;
-        if(rL <= mid) ret+=query(rL,rR,cl(i),L,mid);
-        if(mid < rR) ret+=query(rL,rR,cr(i),mid+1,R);
+        int mid = L + R >> 1, ret = 0;
+        if(rL <= mid) ret += query(rL, rR, cl(i), L, mid);
+        if(mid < rR ) ret += query(rL, rR, cr(i), mid + 1, R);
         return ret;
     }
-    void update(int rL, int rR, int val, int i=1, int L=1, int R=-1) {
-        if(R == -1) R=n;
-        push(i,L,R);
-        if(rL <= L && R <= rR) {
-            tag[i]=val;
-            return;
-        }
-        int mid=(L+R)>>1;
-        if(rL <= mid) update(rL,rR,val,cl(i),L,mid);
-        if(mid < rR) update(rL,rR,val,cr(i),mid+1,R);
-        pull(i,L,R);
+    void update(int rL, int rR, int val, int i = 1, int L = 1, int R = -1) {
+        if(R == -1) R = n;
+        push(i, L, R);
+        if(rL <= L && R <= rR) return void(tag[i] = val);
+        int mid = L + R >> 1;
+        if(rL <= mid) update(rL, rR, val, cl(i), L, mid);
+        if(mid < rR ) update(rL, rR, val, cr(i), mid + 1, R);
+        pull(i, L, R);
     }
-    void cover(int rL, int rR, int val, int i=1, int L=1, int R=-1) {
-        if(R == -1) R=n;
-        push(i,L,R);
-        if(rL <= L && R <= rR) {
-            cov[i]=val;
-            return;
-        }
-        int mid=(L+R)>>1;
-        if(rL <= mid) cover(rL,rR,val,cl(i),L,mid);
-        if(mid < rR) cover(rL,rR,val,cr(i),mid+1,R);
-        pull(i,L,R);
+    void cover(int rL, int rR, int val, int i = 1, int L = 1, int R = -1) {
+        if(R == -1) R = n;
+        push(i, L, R);
+        if(rL <= L && R <= rR) return void(cov[i] = val);
+        int mid = L + R >> 1;
+        if(rL <= mid) cover(rL, rR, val, cl(i), L, mid);
+        if(mid < rR ) cover(rL, rR, val, cr(i), mid + 1, R);
+        pull(i, L, R);
     }
 };
 /*  Test Case:
