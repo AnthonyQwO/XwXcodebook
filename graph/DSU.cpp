7116b1bce96ee160b71896796f583fcdcc55e635
AnthonyQwO <anthonyaa0423@gmail.com>
2024-05-24 12:55:26 +0800

    Update Ocean cup version.

--- graph/DSU.cpp
+++ graph/DSU.cpp
@@ -17,23 +17,3 @@ struct DSU {// 並查集
         return true;
     }
 };
-int MST(int n, int m,vector<tuple<int, int, int>> &edge){ //0 base
-    sort(edge.begin(), edge.end());
-    DSU dsu(n+1); // 初始化並查集
-    int res = 0, flag=1; // 最小生成樹邊權和
-    for (auto &[w, u, v] : edge) 
-        if(dsu.Merge(u, v)) {
-            res += w; //合併並統計答案
-            //graph[u].push_back({v,w});
-            //graph[v].push_back({u,w});
-        }
-        //else edges.push_back({w,u,v});
-    return res;
-}
-int main(){
-    int n, m; //點數,邊數
-    cin >> n >> m;
-    vector<tuple<int, int, int>> edge(m);
-    for (auto &[w, u, v] : edge) cin >> u >> v >> w;
-    cout << MST(n, m, edge);
-}
