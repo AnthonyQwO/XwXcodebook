e03bbff35ac65ce5ea08d585dbff77d0fbc692ae
FSZ560 <gray930522@gmail.com>
2024-11-14 02:03:14 +0800

    edit CircleCover : Rename type D to ld

--- geometry/CircleCover.cpp
+++ geometry/CircleCover.cpp
@@ -1,29 +1,29 @@
 #define N 1021
-#define D long double
+#define ld long double
 struct CircleCover{
   int C; Circle c[ N ]; //填入C(圓數量),c(圓陣列)
   bool g[ N ][ N ], overlap[ N ][ N ];
   // Area[i] : area covered by at least i circles
-  D Area[ N ];
+  ld Area[ N ];
   void init( int _C ){ C = _C; }
   bool CCinter( Circle& a , Circle& b , Pt& p1 , Pt& p2 ){
     Pt o1 = a.o , o2 = b.o;
-    D r1 = a.r , r2 = b.r;
+    ld r1 = a.r , r2 = b.r;
     if( norm( o1 - o2 ) > r1 + r2 ) return {};
     if( norm( o1 - o2 ) < max(r1, r2) - min(r1, r2) ) return {};
-    D d2 = ( o1 - o2 ) * ( o1 - o2 );
-    D d = sqrt(d2);
+    ld d2 = ( o1 - o2 ) * ( o1 - o2 );
+    ld d = sqrt(d2);
     if( d > r1 + r2 ) return false;
     Pt u=(o1+o2)*0.5 + (o1-o2)*((r2*r2-r1*r1)/(2*d2));
-    D A=sqrt((r1+r2+d)*(r1-r2+d)*(r1+r2-d)*(-r1+r2+d));
+    ld A=sqrt((r1+r2+d)*(r1-r2+d)*(r1+r2-d)*(-r1+r2+d));
     Pt v=Pt( o1.y-o2.y , -o1.x + o2.x ) * A / (2*d2);
     p1 = u + v; p2 = u - v;
     return true;
   }
   struct Teve {
-    Pt p; D ang; int add;
+    Pt p; ld ang; int add;
     Teve() {}
-    Teve(Pt _a, D _b, int _c):p(_a), ang(_b), add(_c){}
+    Teve(Pt _a, ld _b, int _c):p(_a), ang(_b), add(_c){}
     bool operator<(const Teve &a)const
     {return ang < a.ang;}
   }eve[ N * 2 ];
@@ -56,8 +56,8 @@ struct CircleCover{
         if( i != j && g[i][j] ){
           Pt aa, bb;
           CCinter(c[i], c[j], aa, bb);
-          D A=atan2(aa.y - c[i].o.y, aa.x - c[i].o.x);
-          D B=atan2(bb.y - c[i].o.y, bb.x - c[i].o.x);
+          ld A=atan2(aa.y - c[i].o.y, aa.x - c[i].o.x);
+          ld B=atan2(bb.y - c[i].o.y, bb.x - c[i].o.x);
           eve[E ++] = Teve(bb, B, 1);
           eve[E ++] = Teve(aa, A, -1);
           if(B > A) cnt ++;
@@ -69,7 +69,7 @@ struct CircleCover{
         for( int j = 0 ; j < E ; j ++ ){
           cnt += eve[j].add; 
           Area[cnt] += (eve[j].p ^ eve[j + 1].p) * 0.5;
-          D theta = eve[j + 1].ang - eve[j].ang;
+          ld theta = eve[j + 1].ang - eve[j].ang;
           if (theta < 0) theta += 2.0 * pi;
           Area[cnt] +=
             (theta - sin(theta)) * c[i].r*c[i].r * 0.5;
